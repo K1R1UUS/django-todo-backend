26a8f87104fe3f06a2db5d61f3c196a2f9cd2c75
K1R1UUS <kk.pichuginn@gmail.com>
2026-07-29 11:26:38 +0300

    Wire up /api/auth/me/ and /api/auth/change-password/ routes

--- tasks/urls_auth.py
+++ tasks/urls_auth.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views_auth import login_view, logout_view, LogoutAPIView, RegisterAPIView
+from .views_auth import (
+    login_view, logout_view, LogoutAPIView,
+    RegisterAPIView, ProfileAPIView, ChangePasswordAPIView,
+)
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
@@ -9,4 +12,6 @@ urlpatterns = [
     path('logout/token/', LogoutAPIView.as_view(), name='logout_token'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', ProfileAPIView.as_view(), name='profile'),
+    path('change-password/', ChangePasswordAPIView.as_view(), name='change_password'),
 ]
